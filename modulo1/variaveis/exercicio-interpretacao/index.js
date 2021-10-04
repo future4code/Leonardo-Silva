32e8a9a7a083233a0fbcc1d050fd5f8a8a537e2d
Leozbroca <leo.brokka@gmail.com>
2021-10-04 14:43:19 -0300

    Adicionei uma nova pasta para trabalhar na aula de variáveis, dentro dela adicionei mais 4 pastas contendo todos os exercícios do dia.

---
+++ modulo1/variaveis/exercicio-interpretacao/index.js
@@ -0,0 +1,18 @@
+/* Exercícios de interpretação de código
+
+1. No primeiro console.log, será impresso o número 10. Já no segundo,
+   será impresso o número 10, e logo depois o número 5.
+   Ficando assim:
+
+   10
+   10 5
+
+2. O resultado seria o número 10 impresso 3 vezes:
+   10 10 10
+
+3. Para a variável p, alguns nomes melhores seriam "horas" ou 
+"horasDeTrabalho".
+   Para a variável t, alguns nomes melhores seriam "dinheiro" ou 
+"salario"
+
+*/
