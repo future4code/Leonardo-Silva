a4ce9347fcc5a4a6c904a495745f48d95217758b
Leozbroca <leo.brokka@gmail.com>
2021-11-17 21:28:46 -0300

    exercicio de api

---
+++ modulo2/apis-labenusers/apis-labenusers/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import styled from 'styled-components'
+
+const MainDiv = styled.div`
+  display:flex;
+  flex-direction:column;
+`
+
+const MenorDiv = styled.div`
+  display:flex;
+  flex-direction:column;
+`
+
+function App() {
+  return (
+    <MainDiv>
+      <div></div>
+    </MainDiv>
+  );
+}
+
+export default App;
