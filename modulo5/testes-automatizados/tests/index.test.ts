adc427689ecdea4afb3609151e374a775cdc2571
Leozbroca <leo.brokka@gmail.com>
2022-03-16 07:46:56 -0300

    exercicios de testes-automatizados

---
+++ modulo5/testes-automatizados/tests/index.test.ts
@@ -0,0 +1,46 @@
+import { performPurchase } from "../src"
+import { User } from "../src"
+
+describe("Testing Purchases", () => {
+  // TESTE 1
+  test("Testing balance greater than value", () => {
+    const user: User = {
+      name: "Astrodev",
+      balance: 100,
+    };
+
+    const result = performPurchase(user, 40);
+
+    expect(result).toEqual({
+      name: "Astrodev",
+      balance: 60,
+    });
+  });
+
+  //TESTE 2
+  test("Testing balance greater than value", () => {
+    const user: User = {
+      name: "Astrodev",
+      balance: 50,
+    };
+
+    const result = performPurchase(user, 50);
+
+    expect(result).toEqual({
+      ...user,
+      balance: 0,
+    });
+  });
+
+  //TESTE 3
+  test("Testing balance greater than value", () => {
+    const user: User = {
+      name: "Astrodev",
+      balance: 30,
+    };
+
+    const result = performPurchase(user, 50);
+
+    expect(result).not.toBeDefined();
+  });
+});
